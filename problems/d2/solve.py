d7d58ae2e8740c182b5d7eef6906154fc1d58c53
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2016-12-19 17:41:27 -0200

    2: Solve part 2

--- problems/d2/solve.py
+++ problems/d2/solve.py
@@ -6,15 +6,8 @@ import numpy as np
 INSTRUCTIONS = open('input.txt').read().split()
 
 
-TRANSLATE_RIGHT = np.matrix([
-    [1],
-    [0],
-])
-
-TRANSLATE_DOWN = np.matrix([
-    [0],
-    [1],
-])
+TRANSLATE_RIGHT = np.array([1, 0])
+TRANSLATE_DOWN = np.array([0, 1])
 
 TRANSLATE = {
     'R': TRANSLATE_RIGHT,
@@ -29,51 +22,49 @@ def cartesian2digit(position):
     return 1 + x + 3 * y
 
 
-def part1():
-    password = []
-    position = np.matrix([[1], [1]])
-    for line in INSTRUCTIONS:
-        for instruction in line:
-            move = TRANSLATE[instruction]
-            new_position = position + move
-            if not (-1 in new_position or 3 in new_position):
-                position = new_position
-
-        password.append(cartesian2digit(position))
-
-    return ''.join(map(str, password))
-
-
-GRID = np.matrix([
-    [0x0, 0x0, 0x1, 0x0, 0x0],
-    [0x0, 0x2, 0x3, 0x4, 0x0],
-    [0x5, 0x6, 0x7, 0x8, 0x9],
-    [0x0, 0xA, 0xB, 0xC, 0x0],
-    [0x0, 0x0, 0xD, 0x0, 0x0],
-])
-
-
-def get_value(position):
-    if -1 in position or 5 in position:
+def get_value(grid, position):
+    edge = len(grid[0])
+    if -1 in position or edge in position:
         return 0
     else:
-        return GRID.T[position.tolist()][0, 0]
+        col, line = position
+        return grid[line][col]
 
 
-def part2():
+def go(grid, position):
     password = []
-    position = np.matrix([[0], [2]])
     for line in INSTRUCTIONS:
         for instruction in line:
             move = TRANSLATE[instruction]
             new_position = position + move
-            value = get_value(new_position)
+            value = get_value(grid, new_position)
             if value:
                 position = new_position
 
-        password.append(get_value(position))
+        password.append(get_value(grid, position))
 
-    return ''.join(map(str, password))
+    return ''.join(map('{:x}'.format, password)).upper()
+
+
+def part1():
+    grid = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+    return go(grid, np.array([1, 1]))
+
+
+def part2():
+    grid = [
+        [0x0, 0x0, 0x1, 0x0, 0x0],
+        [0x0, 0x2, 0x3, 0x4, 0x0],
+        [0x5, 0x6, 0x7, 0x8, 0x9],
+        [0x0, 0xA, 0xB, 0xC, 0x0],
+        [0x0, 0x0, 0xD, 0x0, 0x0],
+    ]
+
+    return go(grid, np.array([0, 2]))
 
 
 if __name__ == '__main__':
