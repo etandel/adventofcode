0b4b32d8cd886d1f47249183d7df6fc06a870e53
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2016-12-21 16:58:03 -0200

    Refactor tests

--- problems/d11/test_solve.py
+++ problems/d11/test_solve.py
@@ -3,6 +3,12 @@ from solve import (Building, Element, Floor, Generator, Microchip,
                    InvalidMoveError)
 
 
+MICRO1 = Microchip(Element.Pu)
+MICRO2 = Microchip(Element.Sr)
+GEN1 = Generator(Element.Pu)
+GEN2 = Generator(Element.Sr)
+
+
 class TestFloor(unittest.TestCase):
     def assert_is_possible(self, objects, expected):
         assertion = self.assertTrue if expected else self.assertFalse
@@ -34,97 +40,67 @@ class TestFloor(unittest.TestCase):
                                 False)
 
     def test_add_single(self):
-        micro1 = Microchip(Element.Pu)
-        micro2 = Microchip(Element.Sr)
-        gen1 = Generator(Element.Pu)
-        gen2 = Generator(Element.Sr)
-
         # add to empty floor
-        self.assertEqual(Floor().add(micro1), Floor([micro1]))
-        self.assertEqual(Floor().add(gen1), Floor([gen1]))
+        self.assertEqual(Floor().add(MICRO1), Floor([MICRO1]))
+        self.assertEqual(Floor().add(GEN1), Floor([GEN1]))
 
         # add to nonempty floor
-        self.assertEqual(Floor([gen1, gen2, micro2]).add(micro1),
-                         Floor([gen1, gen2, micro2, micro1]))
+        self.assertEqual(Floor([GEN1, GEN2, MICRO2]).add(MICRO1),
+                         Floor([GEN1, GEN2, MICRO2, MICRO1]))
 
-        self.assertEqual(Floor([micro1, gen2, micro2]).add(gen1),
-                         Floor([micro1, gen2, micro2, gen1]))
+        self.assertEqual(Floor([MICRO1, GEN2, MICRO2]).add(GEN1),
+                         Floor([MICRO1, GEN2, MICRO2, GEN1]))
 
         # add member
-        floor = Floor([gen1, gen2, micro2])
-        self.assertEqual(floor.add(micro2), floor)
-        self.assertEqual(floor.add(gen2), floor)
+        floor = Floor([GEN1, GEN2, MICRO2])
+        self.assertEqual(floor.add(MICRO2), floor)
+        self.assertEqual(floor.add(GEN2), floor)
 
     def test_add_multiple(self):
-        micro1 = Microchip(Element.Pu)
-        micro2 = Microchip(Element.Sr)
-        gen1 = Generator(Element.Pu)
-        gen2 = Generator(Element.Sr)
-
-        self.assertEqual(Floor().add([micro1, gen1]),
-                         Floor([micro1, gen1]))
+        self.assertEqual(Floor().add([MICRO1, GEN1]),
+                         Floor([MICRO1, GEN1]))
 
         # add to nonempty floor
-        self.assertEqual(Floor([gen1, gen2]).add([micro1, micro2]),
-                         Floor([gen1, gen2, micro1, micro2]))
+        self.assertEqual(Floor([GEN1, GEN2]).add([MICRO1, MICRO2]),
+                         Floor([GEN1, GEN2, MICRO1, MICRO2]))
 
     def test_remove_single(self):
-        micro1 = Microchip(Element.Pu)
-        micro2 = Microchip(Element.Sr)
-        gen1 = Generator(Element.Pu)
-        gen2 = Generator(Element.Sr)
-
         # remove from empty floor
-        self.assertEqual(Floor().remove(micro1), Floor())
-        self.assertEqual(Floor().remove(gen1), Floor())
+        self.assertEqual(Floor().remove(MICRO1), Floor())
+        self.assertEqual(Floor().remove(GEN1), Floor())
 
         # remove non-member
-        floor = Floor([gen2, micro2])
-        self.assertEqual(floor.remove(micro1), floor)
-        self.assertEqual(floor.remove(gen1), floor)
+        floor = Floor([GEN2, MICRO2])
+        self.assertEqual(floor.remove(MICRO1), floor)
+        self.assertEqual(floor.remove(GEN1), floor)
 
         # remove member
-        self.assertEqual(floor.remove(micro2), Floor([gen2]))
-        self.assertEqual(floor.remove(gen2), Floor([micro2]))
+        self.assertEqual(floor.remove(MICRO2), Floor([GEN2]))
+        self.assertEqual(floor.remove(GEN2), Floor([MICRO2]))
 
     def test_remove_multiple(self):
-        micro1 = Microchip(Element.Pu)
-        micro2 = Microchip(Element.Sr)
-        gen1 = Generator(Element.Pu)
-        gen2 = Generator(Element.Sr)
-
-        self.assertEqual(Floor().remove([micro1, gen1]), Floor())
-        self.assertEqual(Floor([gen2, micro2]).remove([micro1, gen2]),
-                         Floor([micro2]))
+        self.assertEqual(Floor().remove([MICRO1, GEN1]), Floor())
+        self.assertEqual(Floor([GEN2, MICRO2]).remove([MICRO1, GEN2]),
+                         Floor([MICRO2]))
 
 
 class TestBuilding(unittest.TestCase):
     def test_move(self):
-        micro1 = Microchip(Element.Pu)
-        micro2 = Microchip(Element.Sr)
-        gen1 = Generator(Element.Pu)
-        gen2 = Generator(Element.Sr)
+        b = Building([Floor([MICRO1]), Floor(), Floor(), Floor([MICRO2])])
 
-        b = Building([Floor([micro1]), Floor(), Floor(), Floor([micro2])])
+        self.assertRaises(InvalidMoveError, b.move_objects_up, [MICRO2])
+        self.assertRaises(InvalidMoveError, b.move_objects_down, [MICRO1])
 
-        self.assertRaises(InvalidMoveError, b.move_objects_up, [micro2])
-        self.assertRaises(InvalidMoveError, b.move_objects_down, [micro1])
-
-        new_b = b.move_objects_up([micro1]).move_objects_down([micro2])
+        new_b = b.move_objects_up([MICRO1]).move_objects_down([MICRO2])
         expected = Building([Floor(),
-                             Floor([micro1]),
-                             Floor([micro2]),
+                             Floor([MICRO1]),
+                             Floor([MICRO2]),
                              Floor()])
         self.assertEqual(new_b, expected)
 
     def is_possible(self):
-        micro1 = Microchip(Element.Pu)
-        micro2 = Microchip(Element.Sr)
-        gen1 = Generator(Element.Pu)
-        gen2 = Generator(Element.Sr)
-
         possible = Building([Floor(), Floor(), Floor(), Floor()])
-        impossible = Building([Floor([micro1, gen2]),
+        impossible = Building([Floor([MICRO1, GEN2]),
                                Floor(), Floor(), Floor()])
         self.assertTrue(possible.is_possible())
         self.assertFalse(impossible.is_possible())
