ec9048823d56c2f173946f7d2ed7c09b7da64b5b
Elias Tandel <elias.tandel@gmail.com>
2021-12-02 18:29:11 +0100

    Refactor 2015.9.1

--- 2015/d9/src/main.rs
+++ 2015/d9/src/main.rs
@@ -5,78 +5,70 @@ use std::path::Path;
 
 use itertools::Itertools;
 
-type Node = String;
+type Node<'a> = &'a str;
 type Dist = usize;
 
 fn parse_distance(s: &str) -> ((Node, Node), Dist) {
     match s.split_ascii_whitespace().collect::<Vec<&str>>().as_slice() {
-        [from, "to", to, "=", raw_dist] => (
-            (from.to_string(), to.to_string()),
-            raw_dist.parse().unwrap(),
-        ),
+        [from, "to", to, "=", raw_dist] => ((from, to), raw_dist.parse().unwrap()),
         _ => panic!("Invalid distance: {}", s),
     }
 }
 
-fn parse_distances<P>(path: P) -> HashMap<(Node, Node), Dist>
+fn read_input<P>(path: P) -> String
 where
     P: AsRef<Path>,
 {
-    fs::read_to_string(path)
-        .unwrap()
-        .lines()
-        .map(parse_distance)
+    fs::read_to_string(path).unwrap()
+}
+
+fn distances(inp: &str) -> HashMap<(Node, Node), Dist> {
+    inp.lines().map(parse_distance).collect()
+}
+
+fn get_all_nodes<'a>(distances: &'a HashMap<(Node, Node), Dist>) -> HashSet<&'a str> {
+    distances
+        .keys()
+        .flat_map(|(n1, n2)| [n1, n2])
+        .copied()
         .collect()
 }
 
-fn part1() {
-    let distances = parse_distances("input.txt");
-    let nodes: HashSet<_> = distances.keys().flat_map(|(n1, n2)| [n1, n2]).collect();
+fn iter_possible_distances<'a>(
+    distances: &'a HashMap<(Node, Node), Dist>,
+    nodes: &'a HashSet<&'a str>,
+) -> impl Iterator<Item = Dist> + 'a {
+    nodes.iter().permutations(nodes.len()).map(|perm| {
+        let dist = perm
+            .windows(2)
+            .map(|window| match window {
+                [&from, &to] => distances
+                    .get(&(from, to))
+                    .or_else(|| distances.get(&(to, from)))
+                    .unwrap(),
+                _ => panic!("Invalid window!"),
+            })
+            .sum();
+        dist
+    })
+}
 
-    let min_dist: Dist = nodes
-        .iter()
-        .permutations(nodes.len())
-        .map(|perm| {
-            let dist = perm
-                .windows(2)
-                .map(|window| match window {
-                    [from, to] => distances
-                        .get(&(from.to_string(), to.to_string()))
-                        .or_else(|| distances.get(&(to.to_string(), from.to_string())))
-                        .unwrap(),
-                    _ => panic!("Invalid window!"),
-                })
-                .sum();
-            dist
-        })
-        .min()
-        .unwrap();
+fn part1() {
+    let input = read_input("input.txt");
+    let distances = distances(&input[..]);
+    let nodes = get_all_nodes(&distances);
+    // this is O(n!) and could probably be faster by using eulerian paths or circuits,
+    // but the input has only 8 nodes, so who cares? =D
+    let min_dist: Dist = iter_possible_distances(&distances, &nodes).min().unwrap();
 
     println!("{}", min_dist);
 }
 
 fn part2() {
-    let distances = parse_distances("input.txt");
-    let nodes: HashSet<_> = distances.keys().flat_map(|(n1, n2)| [n1, n2]).collect();
-
-    let min_dist: Dist = nodes
-        .iter()
-        .permutations(nodes.len())
-        .map(|perm| {
-            let dist = perm
-                .windows(2)
-                .map(|window| match window {
-                    [from, to] => distances
-                        .get(&(from.to_string(), to.to_string()))
-                        .or_else(|| distances.get(&(to.to_string(), from.to_string())))
-                        .unwrap(),
-                    _ => panic!("Invalid window!"),
-                })
-                .sum();
-            dist
-        })
-        .max()
-        .unwrap();
+    let input = read_input("input.txt");
+    let distances = distances(&input[..]);
+    let nodes = get_all_nodes(&distances);
+    let min_dist: Dist = iter_possible_distances(&distances, &nodes).max().unwrap();
 
     println!("{}", min_dist);
 }
