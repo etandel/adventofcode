20e9493cb4d47a2bb5524692552d51af718004a8
Elias Tandel <elias.tandel@gmail.com>
2021-12-10 10:39:11 +0100

    Solve 2021.10.2

--- 2021/d10/src/main.rs
+++ 2021/d10/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs;
 use std::path::Path;
@@ -16,6 +15,12 @@ where
 
 type Score = u64;
 
+#[derive(Debug)]
+enum Corruption {
+    Invalid(char),
+    Missing(Vec<char>),
+}
+
 fn score_of(c: char) -> Score {
     match c {
         ')' => 3,
@@ -26,7 +31,17 @@ fn score_of(c: char) -> Score {
     }
 }
 
-fn get_score(line: &str) -> Score {
+fn get_expected(c: char) -> char {
+    match c {
+        '(' => ')',
+        '[' => ']',
+        '{' => '}',
+        '<' => '>',
+        _ => panic!("Invalid opening char: {}", c),
+    }
+}
+
+fn get_problem(line: &str) -> Option<Corruption> {
     let mut stack = Vec::with_capacity(line.len());
 
     for c in line.chars() {
@@ -34,38 +49,60 @@ fn get_score(line: &str) -> Score {
             stack.push(c);
         } else {
             let top = stack.pop().unwrap();
-            if let Some(score) = match (top, c) {
-                ('(', ')') => None,
-                ('[', ']') => None,
-                ('{', '}') => None,
-                ('<', '>') => None,
-
-                (_, ')') => Some(3),
-                (_, ']') => Some(57),
-                (_, '}') => Some(1197),
-                (_, '>') => Some(25137),
-                _ => panic!("Invalid pair: ({}, {})", top, c),
-            } {
-                return score;
+            if c != get_expected(top) {
+                return Some(Corruption::Invalid(c));
             }
         }
     }
 
-    0
+    if stack.is_empty() {
+        None
+    } else {
+        let mut missing = Vec::with_capacity(stack.len());
+
+        while let Some(open) = stack.pop() {
+            missing.push(get_expected(open));
+        }
+
+        Some(Corruption::Missing(missing))
+    }
 }
 
 fn part1() {
     let lines = read_lines("input.txt");
     let mut score = 0;
     for line in lines {
-        score += get_score(&line);
+        if let Some(Corruption::Invalid(c)) = get_problem(&line) {
+            score += score_of(c);
+        }
     }
 
     println!("{}", score);
 }
 
 fn part2() {
-    todo!()
+    let lines = read_lines("input.txt");
+    let mut scores = Vec::with_capacity(lines.len());
+    for line in lines {
+        if let Some(Corruption::Missing(missing)) = get_problem(&line) {
+            let mut score: Score = 0;
+            for c in missing {
+                score = score.saturating_mul(5).saturating_add(match c {
+                    ')' => 1,
+                    ']' => 2,
+                    '}' => 3,
+                    '>' => 4,
+                    _ => panic!("Invalid missing: {}", c),
+                });
+            }
+
+            scores.push(score);
+        }
+    }
+
+    scores.sort();
+
+    println!("{}", scores[scores.len() / 2]);
 }
 
 fn main() {
