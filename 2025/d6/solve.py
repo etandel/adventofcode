91f999250d8801e094fa8981c686988d4b7548f8
Elias Tandel <elias.tandel@gmail.com>
2025-12-20 17:59:19 +0100

    add 2025.6

--- 2025/d6/solve.py
+++ 2025/d6/solve.py
@@ -1,6 +1,8 @@
 import sys
+import string
 from functools import reduce
 from operator import add, mul
+from typing import Callable
 
 OPS = {
     "+": add,
@@ -29,8 +31,56 @@ def part1(file):
     print(total)
 
 
+class Problem:
+    def __init__(self):
+        self.vals: list[int] = []
+        self.op = ""
+
+    def add_val(self, v: int):
+        self.vals.append(v)
+
+    def set_op(self, op: str):
+        self.op = op
+
+    def eval(self) -> int:
+        return apply(self.op, self.vals)
+
+    def __str__(self) -> str:
+        return f" {self.op} ".join(map(str, self.vals))
+
+
 def part2(file):
-    raise NotImplementedError()
+    data = [list(l.strip("\n")) for l in file]
+    ncols = len(data[0])
+
+    problems = []
+    problem = Problem()
+    for i in range(ncols-1, -1, -1):
+        digits = []
+        all_spaces = True
+        for rowi, row in enumerate(data):
+            c = row[i]
+            if c in OPS:
+                all_spaces = False
+                problem.set_op(c)
+            elif c in string.digits:
+                all_spaces = False
+                digits.append(c)
+            elif c == " ":
+                pass
+            else:
+                raise ValueError(f"unknown char {c} at ({rowi}, {i})")
+
+        if digits:
+            problem.add_val(int("".join(digits)))
+
+        if all_spaces:
+            problems.append(problem)
+            problem = Problem()
+
+    problems.append(problem)
+
+    print(sum(map(Problem.eval, problems))) 
 
 
 def main():
